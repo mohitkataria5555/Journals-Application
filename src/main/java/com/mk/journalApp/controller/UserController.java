a8115028c71e6bc073e28fa1159f8f6bc94240eb
mohitkataria5555 <mohitkataria5555@gmail.com>
2024-11-02 19:58:28 +0530

    updating spring security for users

--- src/main/java/com/mk/journalApp/controller/UserController.java
+++ src/main/java/com/mk/journalApp/controller/UserController.java
@@ -1,18 +1,15 @@
 package com.mk.journalApp.controller;
 
+import com.mk.journalApp.Repository.UserRepository;
 import com.mk.journalApp.entity.User;
 import com.mk.journalApp.service.UserService;
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/user")
@@ -20,8 +17,8 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-
-
+    @Autowired
+    private UserRepository userRepository;
     @PutMapping
     public ResponseEntity<?> updateUser(@RequestBody User user) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -30,6 +27,14 @@ public class UserController {
         userInDb.setUsername(user.getUsername());
         userInDb.setPassword(user.getPassword());
         userService.saveNewUser(userInDb);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<?> deleteUser(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        userRepository.deleteByUsername(username);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
